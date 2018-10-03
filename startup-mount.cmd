9f1a7301da52c0ba9772498e144adc0ac4dc2d5e
Socrates Chouridis <socratesx@protonmail.com>
2018-10-04 01:38:48 +0300

    Generify the file
    Pay attention to the correct path of the script directory. Remember to replace the YOUR_PATH_TO_SCRIPT_DIR with the directory where you saved the script.

--- startup-mount.cmd
+++ startup-mount.cmd
@@ -1,2 +1,2 @@
-Powershell -Command Set-ExecutionPolicy -scope LocalMachine -ExecutionPolicy Unrestricted > C:\PS_Scripts\startuplog.txt 2>&1
-Powershell C:\PS_Scripts\mount-virtual-disks.ps1 >> C:\PS_Scripts\startuplog.txt 2>&1
+Powershell -Command Set-ExecutionPolicy -scope LocalMachine -ExecutionPolicy Unrestricted > YOUR_PATH_TO_SCRIPT_DIR\startuplog.txt 2>&1
+Powershell YOUR_PATH_TO_SCRIPT_DIR\mount-virtual-disks.ps1 >> YOUR_PATH_TO_SCRIPT_DIR\startuplog.txt 2>&1
